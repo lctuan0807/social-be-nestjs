cbe8a9101aefac4efdf2deefd0d45d402975519a
Tuan Le <lctuan.0807@gmail.com>
2026-06-26 19:24:35 +0700

    connect to postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available everywhere
+    }),
+    TypeOrmModule.forRoot({
+     type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'my_database',
+      synchronize: process.env.NODE_ENV !== 'production', // Disable in production!
+      entities: [],
+      migrations: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
